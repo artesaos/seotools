cb57bac13c203fef9283068010ef19c57d53261f
Eduardo Bizarro <edbizarro@gmail.com>
2016-04-29 20:40:39 -0300

    binds tested

--- tests/SEOTools/SEOToolsServiceProviderTest.php
+++ tests/SEOTools/SEOToolsServiceProviderTest.php
@@ -9,13 +9,39 @@ class SEOToolsServiceProviderTest extends BaseTest
 {
     /**
      * Verify if classes are in service container.
+     *
+     * @dataProvider bindsListProvider
+     * @param string $contract
+     * @param string $concreteClass
      */
-    public function test_container_are_provided()
+    public function test_container_are_provided($contract, $concreteClass)
     {
         $this->assertInstanceOf(
-            'Artesaos\SEOTools\Contracts\MetaTags',
-            $this->app['Artesaos\SEOTools\SEOMeta']
+            $contract,
+            $this->app[$concreteClass]
         );
+    }
+
+    public function bindsListProvider()
+    {
+        return [
+            [
+                'Artesaos\SEOTools\Contracts\MetaTags',
+                'Artesaos\SEOTools\SEOMeta'
+            ],
+            [
+                'Artesaos\SEOTools\Contracts\OpenGraph',
+                'Artesaos\SEOTools\OpenGraph'
+            ],
+            [
+                'Artesaos\SEOTools\Contracts\SEOTools',
+                'Artesaos\SEOTools\SEOTools'
+            ],
+            [
+                'Artesaos\SEOTools\Contracts\TwitterCards',
+                'Artesaos\SEOTools\TwitterCards'
+            ]
+        ];
     }
 
 }
