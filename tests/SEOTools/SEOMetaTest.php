ccbca5f9c076f422251132a3e9dcebaadc11d81d
Eduardo Bizarro <edbizarro@gmail.com>
2016-05-01 00:33:01 -0300

    finished SEOMeta tests

--- tests/SEOTools/SEOMetaTest.php
+++ tests/SEOTools/SEOMetaTest.php
@@ -33,7 +33,7 @@ class SEOMetaTest extends BaseTest
     public function test_set_title_with_append_default()
     {
         $fullTitle = 'Kamehamehaaaaaaaa - It\'s Over 9000!';
-        $fullHeader = "<title>".$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader = '<title>'.$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
 
         $this->seoMeta->setTitle('Kamehamehaaaaaaaa');
 
@@ -44,7 +44,7 @@ class SEOMetaTest extends BaseTest
     public function test_set_title_without_append_default()
     {
         $fullTitle = 'Kamehamehaaaaaaaa';
-        $fullHeader = "<title>".$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader = '<title>'.$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
 
         $this->seoMeta->setTitle($fullTitle, false);
 
@@ -55,7 +55,7 @@ class SEOMetaTest extends BaseTest
     public function test_set_default_title()
     {
         $fullTitle = 'Kamehamehaaaaaaaa';
-        $fullHeader = "<title>".$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader = '<title>'.$fullTitle."</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
 
         $this->seoMeta->setTitleDefault($fullTitle);
         $this->assertEquals($fullTitle, $this->seoMeta->getDefaultTitle());
@@ -78,12 +78,16 @@ class SEOMetaTest extends BaseTest
     public function test_set_description()
     {
         $description = 'Kamehamehaaaaaaaa';
-        $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"".$description."\">";
+        $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"".$description.'">';
 
         $this->seoMeta->setDescription($description);
 
         $this->assertEquals($description, $this->seoMeta->getDescription());
         $this->assertEquals($fullHeader, $this->seoMeta->generate());
+
+        $this->seoMeta->setDescription(false);
+        $this->assertNull($this->seoMeta->getDescription());
+
     }
 
     public function test_set_keywords()
@@ -108,15 +112,27 @@ class SEOMetaTest extends BaseTest
 
         $this->assertEquals($fullHeader, $this->seoMeta->generate());
         $this->assertEquals('masenko, makankosappo', implode($this->seoMeta->getKeywords(), ', '));
+
+        $this->seoMeta->addKeyword(['kienzan','tayoken']);
+
+        $this->assertEquals('kienzan, tayoken, masenko, makankosappo', implode($this->seoMeta->getKeywords(), ', '));
+
     }
 
     public function test_remove_metatag()
     {
         $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
 
-        $this->seoMeta->addMeta('custom-meta', 'value', 'test');
+        $this->seoMeta->addMeta('no-content');
         $this->seoMeta->addMeta(['custom-meta' => 'value']);
+        $this->seoMeta->addMeta('custom-meta', 'value', 'test');
+
+        $fullHeaderWithTags = $fullHeader."\n<meta test=\"custom-meta\" content=\"value\">";
+
+        $this->assertEquals($fullHeaderWithTags, $this->seoMeta->generate());
+
         $this->seoMeta->removeMeta('custom-meta');
+        $this->seoMeta->removeMeta('no-content');
 
         $this->assertEquals($fullHeader, $this->seoMeta->generate());
         $this->assertEquals([], $this->seoMeta->getMetatags());
@@ -132,5 +148,62 @@ class SEOMetaTest extends BaseTest
 
         $this->assertEquals($fullHeader, $this->seoMeta->generate());
         $this->assertEquals($canonical, $this->seoMeta->getCanonical());
+    }
+
+    public function test_set_next()
+    {
+        $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader .= "\n<link rel=\"next\" href=\"http://localhost\"/>";
+        $next = 'http://localhost';
+
+        $this->seoMeta->setNext($next);
+
+        $this->assertEquals($fullHeader, $this->seoMeta->generate());
+        $this->assertEquals($next, $this->seoMeta->getNext());
+    }
+
+    public function test_set_prev()
+    {
+        $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader .= "\n<link rel=\"prev\" href=\"http://localhost\"/>";
+        $prev = 'http://localhost';
+
+        $this->seoMeta->setPrev($prev);
+
+        $this->assertEquals($fullHeader, $this->seoMeta->generate());
+        $this->assertEquals($prev, $this->seoMeta->getPrev());
+    }
+
+    public function test_set_alternate_languages()
+    {
+        $fullHeader = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $fullHeader .= "\n<link rel=\"alternate\" hreflang=\"en\" href=\"http://localhost\"/>";
+        $lang = 'en';
+        $langUrl = 'http://localhost';
+
+        $expectedLangs = [['lang' => $lang, 'url' => $langUrl]];
+        $this->seoMeta->addAlternateLanguage($lang, $langUrl);
+
+        $this->assertEquals($fullHeader, $this->seoMeta->generate());
+        $this->assertEquals($expectedLangs, $this->seoMeta->getAlternateLanguages());
+
+        $this->seoMeta->addAlternateLanguages($expectedLangs);
+
+        $this->assertEquals(array_merge($expectedLangs,$expectedLangs), $this->seoMeta->getAlternateLanguages());
+    }
+
+    public function test_set_reset()
+    {
+        $expected = "<title>It's Over 9000!</title>\n<meta name=\"description\" content=\"For those who helped create the Genki Dama\">";
+        $this->seoMeta->setDescription('test');
+        $this->seoMeta->addKeyword('test');
+        $this->seoMeta->setNext('test');
+        $this->seoMeta->setPrev('test');
+        $this->seoMeta->setCanonical('test');
+        $this->seoMeta->addMeta('test');
+        $this->seoMeta->addAlternateLanguage('test', 'test');
+        $this->seoMeta->reset();
+
+        $this->assertEquals($expected, $this->seoMeta->generate());
     }
 }
