6bbd0ff23d8746901e7856aaa65d546f008ff0de
Paul Klimov <klimov.paul@gmail.com>
2019-09-05 16:46:03 +0300

    add facades PHPDoc (#175)

--- src/SEOTools/Facades/TwitterCard.php
+++ src/SEOTools/Facades/TwitterCard.php
@@ -4,8 +4,26 @@ namespace Artesaos\SEOTools\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * TwitterCard is a facade for the `TwitterCards` implementation access.
+ *
+ * @see \Artesaos\SEOTools\Contracts\TwitterCards
+ *
+ * @method static string generate(bool $minify = false)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards addValue(string $key, array|string $value)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setType(string $type)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setTitle(string $title)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setSite(string $site)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setDescription(string $description)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setUrl(string $url)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards addImage(string|array $image)
+ * @method static \Artesaos\SEOTools\Contracts\TwitterCards setImages(array $images)
+ */
 class TwitterCard extends Facade
 {
+    /**
+     * {@inheritdoc}
+     */
     protected static function getFacadeAccessor()
     {
         return 'seotools.twitter';
