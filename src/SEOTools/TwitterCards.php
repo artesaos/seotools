a1942fc7dc07b6a6b2fe6f0355b2fcdfff927fe4
Ugur Aydogdu <ugur.aydogdu@epigra.com>
2020-01-28 15:54:55 +0300

    Update TwitterCards.php

--- src/SEOTools/TwitterCards.php
+++ src/SEOTools/TwitterCards.php
@@ -83,7 +83,7 @@ class TwitterCards implements TwitterCardsContract
      */
     private function makeTag($key, $value)
     {
-        $value = preg_replace(array("http-equiv", "url="),"", $value);
+        $value = str_replace(['http-equiv=', 'url='], '', $value);
         return '<meta name="'.$this->prefix.strip_tags($key).'" content="'.strip_tags($value).'" />';
     }
 
