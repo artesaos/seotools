797edd206a89e042744d3a0e94c813d4811c2aa3
J-Brk <jobvdbroek@gmail.com>
2023-03-23 16:10:04 +0100

    Update seotools.php formatting

--- src/resources/config/seotools.php
+++ src/resources/config/seotools.php
@@ -8,6 +8,7 @@ return [
         /*
          * The default configurations to be used by the meta generator.
          */
+
         'defaults'       => [
             'title'        => "It's Over 9000!", // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
@@ -17,9 +18,11 @@ return [
             'canonical'    => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'robots'       => false, // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
         ],
+        
         /*
          * Webmaster tags are always added.
          */
+
         'webmaster_tags' => [
             'google'    => null,
             'bing'      => null,
@@ -31,10 +34,12 @@ return [
 
         'add_notranslate_class' => false,
     ],
+    
     'opengraph' => [
         /*
          * The default configurations to be used by the opengraph generator.
          */
+
         'defaults' => [
             'title'       => 'Over 9000 Thousand!', // set false to total remove
             'description' => 'For those who helped create the Genki Dama', // set false to total remove
@@ -44,19 +49,23 @@ return [
             'images'      => [],
         ],
     ],
+    
     'twitter' => [
         /*
          * The default values to be used by the twitter cards generator.
          */
+
         'defaults' => [
             //'card'        => 'summary',
             //'site'        => '@LuizVinicius73',
         ],
     ],
+    
     'json-ld' => [
         /*
          * The default configurations to be used by the json-ld generator.
          */
+
         'defaults' => [
             'title'       => 'Over 9000 Thousand!', // set false to total remove
             'description' => 'For those who helped create the Genki Dama', // set false to total remove
